8168012651eeaf26bb4ff97ef067f351eae1e95a
Andrea Censi <andrea@cds.caltech.edu>
2010-12-25 13:24:10 -0800

    cleanup

--- src/snp_geometry/geometry_contracts.py
+++ src/snp_geometry/geometry_contracts.py
@@ -15,29 +15,20 @@ fail('unit_length', np.array([0, 2]))
 new_contract('direction', 'array[3], unit_length')
 new_contract('unit_quaternion', 'array[4], unit_length')
 
-@contracts(x='array[3x3], unit_length')
-def direction(x):
-    return np.allclose(np.linalg.det(x), 1)
-
-
 @new_contract
 @contracts(x='array[NxN],N>0')
 def orthogonal(x):
-#    N = orthogonal.context['N']
     N = x.shape[0]
     I = np.eye(N) 
     rtol = 10E-10
     atol = 10E-7
     np.testing.assert_allclose(I, np.dot(x, x.T), rtol=rtol, atol=atol)
     np.testing.assert_allclose(I, np.dot(x.T, x), rtol=rtol, atol=atol)
-#    M = [np.dot(x, x.T), np.dot(x.T, x)]
-#    return (np.allclose(I, M[0]) and
-#            np.allclose(I, M[1]))  
 
 @new_contract
 @contracts(x='array[3x3], orthogonal')
 def rotation_matrix(x):
     det = np.linalg.det(x)
-    if not np.allclose(det, 1):
-        raise ValueError('Determinant is not 1 (%f)' % det)
+    np.testing.assert_allclose(det, 1)
+    
     
--- src/snp_geometry/unittests/random_geometry_tests.py
+++ src/snp_geometry/unittests/random_geometry_tests.py
@@ -1,14 +1,20 @@
-import numpy
 import unittest
 
-from snp_geometry import random_rotation, random_quaternion
+from snp_geometry import random_rotation, random_quaternion, random_direction
 
+N = 100
+    
 class GeometryTests(unittest.TestCase):
     
-    def test_random_rotations(self):
-        for i in range(50):
+    def test_random_quaternions(self):
+        for i in range(N):
             q = random_quaternion()
         
-        for i in range(50):
+    def test_random_rotations(self):
+        for i in range(N):
             R = random_rotation()
     
+    def test_random_directions(self):
+        for i in range(N):
+            q = random_direction()
+        
