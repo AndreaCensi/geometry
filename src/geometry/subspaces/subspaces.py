1c6b9127e22c216e185dc700f158ca7250d43f96
Andrea Censi <andrea@cds.caltech.edu>
2012-01-17 23:51:06 -0800

    subspaces

---
+++ src/geometry/subspaces/subspaces.py
@@ -0,0 +1,39 @@
+import numpy as np
+from numpy.testing import *
+from contracts import *
+
+
+@contract(A='array[NxK]')
+def normalize_columns(A):
+    A = A.copy()
+    n, k = A.shape
+    for j in range(k):
+        A[:, j] = A[:, j] / np.linalg.norm(A[:, j])
+    return A
+
+
+@contract(A='array[NxK]')
+def proj_from_subspace(A):
+    An = normalize_columns(A)
+    return np.dot(An, An.T)
+
+
+def get_random_proj(n, k):
+    A = np.random.randn(n, k)
+    return proj_from_subspace(A)
+
+
+@contract(P='array[NxN]')
+def assert_projection(P):
+    assert_allclose(P.T, P)
+    assert_allclose(np.dot(P, P), P)
+
+n = 6
+P1 = get_random_proj(n, 1)
+P2 = get_random_proj(n, 1)
+
+C = np.dot(P1, P2)
+assert_projection(P1)
+assert_projection(P2)
+
+assert_projection(C)
