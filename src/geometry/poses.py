fixed numeric precision bug

src/geometry/poses.py
@@ -214,7 +214,7 @@ def SE3_from_SE2(pose):
 
 
 @contract(pose='SE3', returns='SE2')
-def SE2_from_SE3(pose, check_exact=True):
+def SE2_from_SE3(pose, check_exact=True, z_atol=1e-6):
     ''' 
         Projects a pose in SE3 to SE2.
     
@@ -223,13 +223,21 @@ def SE2_from_SE3(pose, check_exact=True):
     rotation, translation = rotation_translation_from_pose(pose)
     axis, angle = axis_angle_from_rotation(rotation)
     if check_exact:
+        sit = '\n pose %s' % pose
+        sit += '\n axis: %s' % axis
+        sit += '\n angle: %s' % angle
+
         err_msg = ('I expect that z=0 when projecting to SE2 '
                    '(check_exact=True).')
-        assert_allclose(translation[2], 0, err_msg=err_msg)
+        err_msg += sit
+
+        assert_allclose(translation[2], 0, atol=z_atol, err_msg=err_msg)
         # normalize angle z
         axis2 = axis * np.sign(axis[2])
         err_msg = ('I expect that the rotation is around [0,0,1] '
                   'when projecting to SE2 (check_exact=True).')
+        err_msg += sit
+
         assert_allclose(axis2, [0, 0, 1],
                         rtol=GeometryConstants.rtol_SE2_from_SE3,
                         atol=GeometryConstants.rtol_SE2_from_SE3, # XXX

src/geometry/rotations.py
@@ -315,7 +315,16 @@ def axis_angle_from_rotation(R):
         v = np.array([R[2, 1] - R[1, 2],
                    R[0, 2] - R[2, 0],
                    R[1, 0] - R[0, 1]])
-        axis = (1 / (2 * np.sin(angle))) * v
+
+        computer_with_infinite_precision = False
+        if computer_with_infinite_precision:
+            axis = (1 / (2 * np.sin(angle))) * v
+        else:
+            # OK, the formula above gives (theoretically) the correct answer
+            # but it is imprecise if angle is small (dividing by a very small
+            # quantity). This is way better...
+            axis = (v * np.sign(angle)) / np.linalg.norm(v)
+
         return axis, angle
 
 