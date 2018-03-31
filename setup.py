93bad0f3a3dcb3bdf27d167756b2561131f1d702
Andrea Censi <acensi@ethz.ch>
2018-03-31 11:34:30 +0200

    bump

--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
 from setuptools import setup, find_packages
 
-version = '1.4dev1'
+
+def get_version(filename):
+    import ast
+    version = None
+    with open(filename) as f:
+        for line in f:
+            if line.startswith('__version__'):
+                version = ast.parse(line).body[0].value.s
+                break
+        else:
+            raise ValueError('No version found in %r.' % filename)
+    if version is None:
+        raise ValueError(filename)
+    return version
+
+
+version = get_version(filename='src/geometry/__init__.py')
 
 setup(name='PyGeometry',
       version=version,
