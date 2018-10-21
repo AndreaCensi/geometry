18b514e83fb6995931fac4782aee081fa2ba0d14
Andrea Censi <acensi@ethz.ch>
2018-10-21 20:06:42 +0200

    fix

--- src/geometry/manifolds/tests/pickling_tests.py
+++ src/geometry/manifolds/tests/pickling_tests.py
@@ -6,9 +6,9 @@ from geometry.manifolds.tests import for_all_manifolds
 
 @for_all_manifolds
 def check_manifold_pickable(M):
-    from six import StringIO
+    from six import BytesIO
 
-    s = StringIO()
+    s = BytesIO()
     pickle.dump(M, s)
 
     # TODO: read
