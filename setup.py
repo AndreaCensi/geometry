4ca2dd527078effdba911a55f82133f257ac8694
Andrea Censi <acensi@ethz.ch>
2021-01-04 17:23:32 +0000

    templates

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from setuptools import setup
+import json
+import os
+
+from setuptools import find_packages, setup
 
 
 def get_version_from_source(filename):
@@ -16,11 +19,13 @@ def get_version_from_source(filename):
         raise ValueError(filename)
     return version
 
-
-import yaml
-
-with open("project.pp1.yaml") as f:
-    data = yaml.load(f, Loader=yaml.Loader)
+if os.path.exists("project.pp1.json"):
+    with open("project.pp1.json") as f:
+        data = json.load(f)
+else:
+    import yaml
+    with open("project.pp1.yaml") as f:
+        data = yaml.load(f, Loader=yaml.Loader)
 
 install_requires = data["install_requires"]
 tests_require = data["tests_require"]
@@ -28,10 +33,15 @@ tests_require = data["tests_require"]
 src = data["srcdir"]
 console_scripts = [f"{k} = {v}" for k, v in data["console_scripts"].items()]
 package_name = data["package_name"]
-packages = data["modules"]
-main_package = packages[0]
-version = get_version_from_source(f"{src}/{main_package}/__init__.py")
+main_module = data["modules"][0]
+version = get_version_from_source(f"{src}/{main_module}/__init__.py")
 
+modules = []
+for _ in data["modules"]:
+    modules.append(_)
+    modules.extend(_ + "." + x for x in find_packages(f"{src}/{_}"))
+
+print("found modules", modules)
 # setup package
 params = dict(
     name=package_name,
@@ -41,7 +51,7 @@ params = dict(
     tests_require=tests_require,
     install_requires=install_requires,
     package_dir={"": src},
-    packages=data["modules"],
+    packages=modules,
     long_description="",
     version=version,
     entry_points={"console_scripts": console_scripts},
@@ -49,4 +59,4 @@ params = dict(
 
 setup(**params)
 
-# sigil eb9c1d7b9c70de6abb2a4a14d0c49253
+# sigil d31d784c6d6ce211111ff940ad735e63
