08e095e0a42e81d8e3a2005f4ac319e0457e8a40
Andrea Censi <acensi@ethz.ch>
2021-01-04 21:36:16 +0000

    templates

--- setup.py
+++ setup.py
@@ -7,21 +7,21 @@ with open("setup.json") as f:
 
 # setup package
 params = dict(
-    name=data['package_name'],
+    name=data["package_name"],
     author=data["author"],
     author_email=data["author_email"],
     url=data["url"],
-    tests_require=data['tests_require'],
-    install_requires=data['install_requires'],
-    package_dir={"": data['srcdir']},
-    packages=data['modules'],
+    tests_require=data["tests_require"],
+    install_requires=data["install_requires"],
+    package_dir={"": data["srcdir"]},
+    packages=data["modules"],
     long_description="",
-    version=data['version'],
-    entry_points=data['entry_points'],
-    zip_safe=data['zip_safe'],
-    include_package_data=data['include_package_data'],
+    version=data["version"],
+    entry_points=data["entry_points"],
+    zip_safe=data["zip_safe"],
+    include_package_data=data["include_package_data"],
 )
 
 setup(**params)
 
-# sigil 785edf5fce85417b0c8d4297bedddf78
+# sigil db3b1eaa6e910979b80cf8826d500c83
