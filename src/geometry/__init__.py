0029b58f6ce9d5a4a5b4c6402cb465145c04ba44
Andrea Censi <acensi@ethz.ch>
2021-04-23 17:03:26 +0200

    Bump to version 7.1.2104231703

--- src/geometry/__init__.py
+++ src/geometry/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "7.1.2104191743"
-__date__ = "2021-04-19T17:43:07.925521"
+__version__ = "7.1.2104231703"
+__date__ = "2021-04-23T17:03:26.516341"
 # If True, additional checks are done at runtime
 from zuper_commons.logs import ZLogger
 
