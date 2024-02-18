0290ffa8f66820285010ad8486fa2d0ef831837c
Andrea Censi <AndreaCensi@users.noreply.github.com>
2024-02-18 12:49:04 +0100

    [ci skip]

--- src/geometry/utils/numpy_backport.py
+++ src/geometry/utils/numpy_backport.py
@@ -1,5 +1,3 @@
-from numpy.ma.core import allclose
-
 import numpy as np
 
 __all__ = [
@@ -8,12 +6,12 @@ __all__ = [
 ]
 
 try:
-    from numpy.testing.utils import assert_allclose  # @UnusedImport
+    from numpy.testing import assert_allclose
 except ImportError:
 
     def assert_allclose(actual, desired, rtol=1e-7, atol=0, err_msg="", verbose=True):
         """Backporting assert_allclose from Numpy 1.5 to 1.4"""
-        from numpy.testing.utils import assert_array_compare  # @UnresolvedImport
+        from numpy.testing import assert_array_compare
 
         def compare(x, y):
             return np.allclose(x, y, rtol=rtol, atol=atol)
@@ -23,7 +21,7 @@ except ImportError:
         assert_array_compare(compare, actual, desired, err_msg=str(err_msg), verbose=verbose, header=header)
 
 
-def check_allclose(actual, desired, rtol=1e-7, atol=0, err_msg="", verbose=True):  # @UnusedVariable
-    if not allclose(actual, desired, rtol=rtol, atol=atol):
+def check_allclose(actual, desired, rtol: float = 1e-7, atol: float = 0, err_msg: str = "", verbose: bool = True) -> None:
+    if not np.allclose(actual, desired, rtol=rtol, atol=atol):
         err_msg += "\n%s != %s " % (actual, desired)
         raise Exception(err_msg)
